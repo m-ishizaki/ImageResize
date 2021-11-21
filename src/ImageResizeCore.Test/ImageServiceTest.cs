14640c3de0f597ee01ad2d45a7a7aaeac60c863e
is12t <love.ms.is12t@hotmail.co.jp>
2021-11-21 22:28:18 +0900

    add ImageServ\ce class

---
+++ src/ImageResizeCore.Test/ImageServiceTest.cs
@@ -0,0 +1,49 @@
+﻿using ImageResizeCore.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageResizeCore.Test;
+
+internal class ImageServiceTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Bitmap source = new (100, 200);
+
+        IImageService imageService = new ImageService();
+
+        var result = imageService.Resize(source, 50);
+
+        Assert.IsTrue(
+            result.Width == 25
+            && result.Height == 50
+            );
+    }
+
+    [Test]
+    public void Test2()
+    {
+        Bitmap source = new(400, 200);
+
+        IImageService imageService = new ImageService();
+
+        var result = imageService.Resize(source, 50);
+
+        Assert.IsTrue(
+            result.Width == 50
+            && result.Height == 25
+            );
+    }
+
+}
---
+++ src/ImageResizeCore/Services/ImageService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageResizeCore.Services;
+
+internal class ImageService : IImageService
+{
+    public Bitmap Resize(Bitmap source, int longSideLength)
+    {
+        (int height, int width)
+            = (source.Height > source.Width)
+            ? (longSideLength, (int)(source.Width * (longSideLength / (float)source.Height)))
+            : ((int)(source.Height * (longSideLength / (float)source.Width)), longSideLength)
+            ;
+
+        Bitmap result = new(width, height);
+        var g = Graphics.FromImage(result);
+        g.DrawImage(source, 0, 0, width, height);
+        return result;
+    }
+}
+
