5a91fbea330f48c36e7673ff22807eebbd095065
is12t <love.ms.is12t@hotmail.co.jp>
2021-12-26 00:13:41 +0900

    add arguments parser project

---
+++ src/ArgumentsBuilder/ArgumentsBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rksoftware.ArgumentsBuilder;
+
+public static class ArgumentsBuilder
+{
+    public static (IReadOnlyList<string> parameters, IReadOnlyDictionary<string, string> options) Parse(string[] args)
+    {
+        List<string> parameters = new();
+        Dictionary<string, string> options = new();
+        string optionName = "";
+
+        foreach (var arg in args)
+        {
+            if (string.IsNullOrWhiteSpace(optionName))
+            {
+                parameters.Add(arg);
+                continue;
+            }
+            if (arg.StartsWith("-"))
+            {
+                optionName = new string(arg.SkipWhile(c => c != '-').ToArray());
+                options.Add(arg, "");
+                continue;
+            }
+            options[optionName] = arg;
+            optionName = "";
+        }
+
+        return (parameters, options);
+    }
+
+}
+
