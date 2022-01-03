7dd3384676e1275e6db52e36f8ec7e6b31d8a755
is12t <love.ms.is12t@hotmail.co.jp>
2022-01-04 00:18:57 +0900

    add ArgumentsBuilder

--- src/ImageResizeConsole/Program.cs
+++ src/ImageResizeConsole/Program.cs
@@ -2,15 +2,26 @@
 using ImageResizeCore.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Rksoftware.ArgumentsBuilder.Attributes;
+using Rksoftware.ArgumentsBuilder.Hosts;
 
 using IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((_, services) =>
         // 登録
         services
+            .AddSingleton<Service, Service>()
             .AddSingleton<IImageFileResizeService, ImageFileResizeService>()
             .AddSingleton<IImageService, ImageService>()
             .AddSingleton<IFileRepository, FileRepository>()
     )
     .Build();
+host.ApplicationRun<Service, Arguments>(args);
 
-host.Services.GetService<IImageFileResizeService>()!.Resize(args.FirstOrDefault(string.Empty), args.Skip(1).FirstOrDefault(string.Empty), int.TryParse(args.Skip(2).FirstOrDefault(), out var size) ? size : 1368);
+class Service
+{
+    IImageFileResizeService imageFileResizeService;
+    public Service(IImageFileResizeService imageFileResizeService) => (this.imageFileResizeService) = (imageFileResizeService);
+    void Run(Arguments arguments) => imageFileResizeService.Resize(arguments.src ?? string.Empty, arguments.dest ?? string.Empty, (int.TryParse(arguments.size, out var size) ? size : 1368));
+}
+
+record struct Arguments([Parameter] string? src, [Parameter] string? dest, [Parameter] string? size) { }
